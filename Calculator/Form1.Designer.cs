a1ae476a3f9a1c3af006a731a0a34c73a445e6cb
Lukas Prusha <lukasprusha@gmail.com>
2018-04-16 17:58:59 -0700

    Added the ability to input challenge text input. This should handle above the minimum requirements and should be able to handle brackets and multiple expressions.

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.start = new System.Windows.Forms.Button();
             this.challenge_input = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.challenge_start = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.input = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.output = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -55,14 +55,15 @@
             this.challenge_input.TabIndex = 2;
             this.challenge_input.Text = "1+2";
             // 
-            // button1
+            // challenge_start
             // 
-            this.button1.Location = new System.Drawing.Point(496, 45);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Do Your Worst";
-            this.button1.UseVisualStyleBackColor = true;
+            this.challenge_start.Location = new System.Drawing.Point(496, 45);
+            this.challenge_start.Name = "challenge_start";
+            this.challenge_start.Size = new System.Drawing.Size(134, 23);
+            this.challenge_start.TabIndex = 3;
+            this.challenge_start.Text = "Do Your Worst";
+            this.challenge_start.UseVisualStyleBackColor = true;
+            this.challenge_start.Click += new System.EventHandler(this.challenge_start_Click);
             // 
             // listView1
             // 
@@ -83,7 +84,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(630, 491);
             this.Controls.Add(this.listView1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.challenge_start);
             this.Controls.Add(this.challenge_input);
             this.Controls.Add(this.start);
             this.Name = "Calculator";
@@ -97,7 +98,7 @@
 
         private System.Windows.Forms.Button start;
         private System.Windows.Forms.TextBox challenge_input;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button challenge_start;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader input;
         private System.Windows.Forms.ColumnHeader output;
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -13,6 +13,15 @@ namespace Calculator
         private void Start_Click(object sender, EventArgs e)
         {
             var calc = new Calculator_Core();
+            calc.Calculate();
+        }
+
+        private void challenge_start_Click(object sender, EventArgs e)
+        {
+            var input = challenge_input.Text;
+            Console.WriteLine($"Received input: {input}");
+            var calc = new Calculator_Core();
+            calc.Calculate(input);
         }
     }
 }
