3a8e7de1e9ec11ada985e7497a7a18be2888ff1a
LukasGP <lprusha@smartpoints.tech>
2018-04-26 17:59:48 -0700

    Testing order of operations and determining primaries

---
+++ CalculatorTestSuite/Bedmas.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTestSuite
+{
+    [TestClass]
+    public class Bedmas
+    {
+        [TestMethod]
+        public void MultiplicationAndAddition()
+        {
+            // SETUP
+            var calc = new Calculator_Core();
+            var input = "-3+4*-5";
+            Tuple<string, double> expectedResult = new Tuple<string, double>(input,-23);
+
+            // EXECUTE
+            var result = calc.CalculateFromString(input);
+
+            // ASSERT
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void DivisionAndAddition()
+        {
+            // SETUP
+            var calc = new Calculator_Core();
+            var input = "-3+20/-5";
+            Tuple<string, double> expectedResult = new Tuple<string, double>(input, -7);
+
+            // EXECUTE
+            var result = calc.CalculateFromString(input);
+
+            // ASSERT
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod]
+        public void Brackets()
+        {
+            // SETUP
+            var calc = new Calculator_Core();
+            var input = "(5+20)/5";
+            Tuple<string, double> expectedResult = new Tuple<string, double>(input, 5);
+
+            // EXECUTE
+            var result = calc.CalculateFromString(input);
+
+            // ASSERT
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ CalculatorTestSuite/Primary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTestSuite
+{
+    [TestClass]
+    public class Primary
+    {
+        [TestMethod]
+        public void NumericalPrimary()
+        {
+            // SETUP
+            var input = "25";
+            double expectedResult = 25;
+            var calc = new Calculator_Core();
+            calc.SetInput(input);
+
+            // EXECUTE
+            var numericPrimary = calc.Primary();
+
+            // ASSERT
+            Assert.AreEqual(expectedResult, numericPrimary);
+        }
+
+        [TestMethod]
+        public void BracketWithNoClosingPrimary()
+        {
+            // SETUP
+            var input = "(";
+            double expectedResult = 25;
+            var calc = new Calculator_Core();
+            calc.SetInput(input);
+
+            // EXECUTE
+            calc.Primary();
+
+            // ASSERT
+            //Assert.AreEqual(expectedResult, numericPrimary);
+        }
+    }
+}
